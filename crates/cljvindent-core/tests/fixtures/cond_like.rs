c2d5e8d1c6e7180728de4b1382880f0e8d26b412
Panagiotis Koromilias <pkoromilias@yahoo.com>
2026-04-27 11:38:46 +0300

    Minor fix on dependency in tests

--- crates/cljvindent-core/tests/fixtures/cond_like.rs
+++ crates/cljvindent-core/tests/fixtures/cond_like.rs
@@ -1,4 +1,4 @@
-use clj_vindent_engine::{engine::find_aligner, AlignKind, helpers::{get_tree, get_root_node}};
+use cljvindent_core::{engine::find_aligner, AlignKind, helpers::{get_tree, get_root_node}};
 use rstest::fixture;
 
 #[fixture]
